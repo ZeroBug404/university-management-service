755a5d32afccd82ffce674a4ae5a81af0ff906bc
Shimu Mallick <404.zerobug@gmail.com>
2023-06-25 14:14:16 +0600

    [DONE] Faculty & Admin CRUD operation

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { UserRoutes } from '../modules/user/user.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 import { StudentRoutes } from '../modules/student/student.route';
+import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
 
 const router = express.Router();
 
@@ -27,6 +29,15 @@ const moduleRoutes = [
   {
     path: '/students',
     route: StudentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: FacultyRoutes,
+  },
+
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
 ];
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,5 +7,7 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  default_student_password: process.env.DEFAULT_STUDENT_PASSWORD,
+  default_student_pass: process.env.DEFAULT_STUDENT_PASSWORD,
+  default_faculty_pass: process.env.DEFAULT_FACULTY_PASSWORD,
+  default_admin_pass: process.env.DEFAULT_ADMIN_PASSWORD,
 };
